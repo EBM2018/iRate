2689e30ecb9448fccd69bcb7a78b85e036c6cf28
ValentinChion <valentin.chion@gmail.com>
2019-01-23 16:50:38 +0100

    Add comments for API doc for get all documents & get a document

--- backend/src/api/ressources/exams/index.js
+++ backend/src/api/ressources/exams/index.js
@@ -1,8 +1,118 @@
 const { Router } = require('express');
+
 const router = new Router();
 const examController = require('./exam.controller.js');
 
+/**
+ * @api {get} /exam Get all exams
+ * @apiName GetAllExams
+ * @apiGroup Examens
+ * @apiDescription Cette URL affiche un JSON contenant tous les Examens de la BDD
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *[
+ *  {
+ *    "_id": "5a9e7dc7717a690c53650ab1",
+ *    "title": "Examen 1",
+ *    "instruction": "A propos de cet examen, voici une formule qui peut vous intéresser: E=1/2mc²",
+ *    "reminder": "- Les calculatrices sont interdites \n - Tout objet connecté est interdit"
+ *    "author" : {
+ *      "name": "Yolo",
+ *      "surname": "Swag"
+ *    }
+ *    "session": {
+ *      "startDate": "01/01/2019Z8:00:00"
+ *      "endDate": "01/01/2019Z12:00:00"
+ *    }
+ *    "creationTime": "2018-03-06T11:38:47.160Z",
+ *    "__v": 0
+ *  },
+ *  {
+ *    "_id": "5a9e7dc7717a690c57989ab1",
+ *    "title": "Examen 2",
+ *    "instruction": "Bon courage pour votre epreuve !",
+ *    "reminder": "- Tout document autorisés"
+ *    "author" : {
+ *      "name": "Justin",
+ *      "surname": "Bieber"
+ *    }
+ *    "session": {
+ *      "startDate": "02/01/2019Z8:00:00"
+ *      "endDate": "02/01/2019Z10:00:00"
+ *    }
+ *    "creationTime": "2018-04-06T11:38:47.160Z",
+ *    "__v": 0
+ *  }
+ *]
+ */
 router.get('/', examController.getExams);
+
+/**
+ * @api {get} /exam/:id Get an exam
+ * @apiName GetExamById
+ * @apiGroup Examens
+ * @apiDescription Cette URL affiche un JSON contenant un examen en fonctionde son id
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *  *  {
+ *    "_id": "5a9e7dc7717a690c53650ab1",
+ *    "title": "Examen 1",
+ *    "instruction": "A propos de cet examen, voici une formule qui peut vous intéresser: E=1/2mc²",
+ *    "reminder": "- Les calculatrices sont interdites \n - Tout objet connecté est interdit",
+ *    "author" : {
+ *      "name": "Yolo",
+ *      "surname": "Swag"
+ *    },
+ *    "session": {
+ *      "startDate": "01/01/2019T8:00:00"
+ *      "endDate": "01/01/2019T12:00:00"
+ *    },
+ *    "exercices" : [
+ *      {
+ *        "_id": "5a9e7dc7832a690c53650ab1"
+ *        "title": "Exercice 1",
+ *        "estimatedTime": "01:00:00",
+ *        "creationTime": "01/01/2019T07:00:00",
+ *        "questions": [
+ *          {
+ *            "_id": "5a9e7dcjdkfa690c53650ab1",
+ *            "title": "Quelle est la somme de 1+1 ?",
+ *            "scale": 0.5,
+ *            "creationTime": "01/01/2019T07:00:00"
+ *          },
+ *          {
+ *            "_id": "5a9e7jekkfa690c53650ab1",
+ *            "title": "Quelle est la somme de 2+2 ?",
+ *            "scale": 0.75,
+ *            "creationTime": "01/01/2019T07:00:00"
+ *          }
+ *        ],
+ *      },
+ *      {
+ *        "_id": "5a9e7dc7832jfhdc53650ab1"
+ *        "title": "Exercice 2",
+ *        "estimatedTime": "00:30:00",
+ *        "creationTime": "01/01/2019T07:15:00",
+ *        "questions": [
+ *          {
+ *            "_id": "5a9e7dlqskfa690c53650ab1",
+ *            "title": "Quelle est la somme de 2*8 ?",
+ *            "scale": 2,
+ *            "creationTime": "01/01/2019T07:15:31"
+ *          },
+ *          {
+ *            "_id": "5a9e7jsqifa690c53650ab1",
+ *            "title": "Quelle est la somme de 4+8 ?",
+ *            "scale": 2.25,
+ *            "creationTime": "01/01/2019T07:16:05"
+ *          }
+ *        ],
+ *      }
+ *    ]
+ *    "creationTime": "2018-03-06T11:38:47.160Z",
+ *    "__v": 0
+ *  }
+ */
 router.get('/:id', examController.getExamById);
 router.get('/correction/:id', examController.getCorrectionById);
 router.post('/', examController.newExam);
--- backend/src/api/ressources/exercices/index.js
+++ backend/src/api/ressources/exercices/index.js
@@ -2,32 +2,9 @@ const { Router } = require('express');
 
 const router = new Router();
 
-/**
- * @api {get} /users Get all users
- * @apiName GetAllUsers
- * @apiGroup Users
- * @apiDescription Cette URL affiche un JSON contenant tous les users de la BDD
- *
- * @apiSuccessExample {json} Success-Response:
- *[
- *  {
- *    "creationTime": "2018-03-06T11:38:47.160Z",
- *    "_id": "5a9e7dc7717a690c53650ab1",
- *    "name": "Jean-Victor",
- *    "linkappId": "jvhap"
- *    "__v": 0
- *  },
- *  {
- *    "creationTime": "2018-03-06T11:41:13.491Z",
- *    "_id": "5a9e7e591817c20db4ef0e40",
- *    "name": "Philippe JailBreaklivet",
- *    "linkappId": "philJS"
- *    "__v": 0
- *  }
- *]
- */
+//TODO: API DOC: Des exemples sont disponibles dans le fichier exams/index.js
 router.get('/', (req, res) => res.send('Hello, World!'));
-//router.get('/', controller.getAll);
+// router.get('/', controller.getAll);
 
 
 module.exports = router;
