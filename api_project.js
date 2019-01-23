ec5350bc94f6c27469354087d4a3b61b6cc0cda4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-23 08:16:03 +0000

    Deploy EBM2018/iRate to github.com/EBM2018/iRate.git:gh-pages

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "ApiDoc pour le projet Boilerplate",
+  "url": "/api",
+  "sampleUrl": "http://localhost:4000/api",
+  "name": "filrouge-boilerplate-backend",
+  "version": "1.0.0",
+  "description": "Un squelette de projet backend pour le fil rouge de l'option EBM 2018-2019",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-01-23T08:15:34.922Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
