2b1df7d9b8778249ccd0286ce9aa238d9f632cbc
ValentinChion <valentin.chion@gmail.com>
2019-01-23 14:59:40 +0100

    Ajout des routes CRUDs pour les exams, il faut définir les controleurs

--- backend/src/api/ressources/exams/exam.controller.js
+++ backend/src/api/ressources/exams/exam.controller.js
@@ -1,15 +1,72 @@
-const exam = require('./exam.controller');
+/* eslint-disable no-trailing-spaces */
+const exam = require('./exam.model');
 
 const examController = {
   getExams(req, res) {
     return res.status(200).send('It Worked...');
   },
 
+  // TODO: ATTENTION A NE PAS ENVOYER LA CORRECTION D'UNE QUESTION.
   getExamById(req, res) {
-    if(req.params.id) {
+    if (req.params.id) {
       return res.status(200).send(req.params.id);
-    } else return res.status(400).send("Bad Request...");
+    } return res.status(400).send('Bad Request...');
   },
+
+  getCorrectionById(req, res) {
+    if (req.params.id) {
+      return res.status(200).send(req.params.id);
+    } return res.status(400).send('Bad Request...');
+  },
+
+  newExam(req, res) {
+    if (req.body) {
+      return res.status(200).json(req.body);
+    } return res.status(400).send('Bad Request...');
+  },
+
+  newExerciceOfExam(req, res) {
+    if (req.body) {
+      return res.status(200).json(req.body);
+    } return res.status(400).send('Bad Request...');
+  },
+
+  editExercice(req, res) {
+    if (req.body) {
+      return res.status(200).json(req.body);
+    } return res.status(400).send('Bad Request...');
+  },
+
+  newQuestionOfExercice(req, res) {
+    if (req.body) {
+      return res.status(200).json(req.body);
+    } return res.status(400).send('Bad Request...');
+  },
+
+  editQuestionById(req, res) {
+    if (req.body) {
+      return res.status(200).json(req.body);
+    } return res.status(400).send('Bad Request...');
+  },
+
+  deleteExamById(req, res) {
+    if (req.params.id) {
+      return res.status(200).send(req.params.id);
+    } return res.status(400).send('Bad Request...');
+  },
+
+  deleteExerciceById(req, res) {
+    if (req.params.id) {
+      return res.status(200).send(req.params.id);
+    } return res.status(400).send('Bad Request...');
+  },
+
+  deleteQuestionById(req, res) {
+    if (req.params.id) {
+      return res.status(200).send(req.params.id);
+    } return res.status(400).send('Bad Request...');
+  },
+
 };
 
 module.exports = examController;
--- backend/src/api/ressources/exams/index.js
+++ backend/src/api/ressources/exams/index.js
@@ -4,5 +4,17 @@ const examController = require('./exam.controller.js');
 
 router.get('/', examController.getExams);
 router.get('/:id', examController.getExamById);
+router.get('/correction/:id', examController.getCorrectionById);
+router.post('/', examController.newExam);
+router.delete('/', examController.deleteExamById);
+
+router.post('/exercice', examController.newExerciceOfExam);
+router.patch('/exercie', examController.editExercice):
+router.delete('/exercice', examController.deleteExerciceById);
+
+router.post('/exercice/question', examController.newQuestionOfExercice);
+router.patch('/exercice/question', examController.editQuestionById);
+router.delete('/exercice/question', examController.deleteQuestionById);
+
 
 module.exports = router;
