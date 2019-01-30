2d77398fe0b37b3550f4962f21a30281a0f29318
Nans <nans.dumortier-isc.france@ibm.com>
2019-01-30 15:02:10 +0100

    Add middlewares to be able to parse requests and to handle errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const serveStatic = require('serve-static');
+const bodyParser = require('body-parser');
 
 const app = express();
 const server = require('http').Server(app);
@@ -11,7 +12,20 @@ const config = require('./config');
 // setup database connexion
 require('./config/mongoose');
 
+
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+app.use(bodyParser.json());
+
 app.use('/api', require('./api'));
+
+app.use((err, req, res) => {
+  console.error(err.message); // Log error message in our server's console
+  // If err has no specified error code, set error code to 'Internal Server Error (500)'
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).send(err.message);
+});
 
 app.use(serveStatic('./public'));
 
