a2687642f4e5adfbe75e49c542fd344022320df6
Nans <nans.dumortier-isc.france@ibm.com>
2019-02-07 17:06:53 +0100

    Create middlewares for finding exams

--- backend/src/api/ressources/exams/middlewares.js
+++ backend/src/api/ressources/exams/middlewares.js
@@ -1,4 +1,6 @@
 const ExamData = require('../../../services/exam/data');
+const ExerciceData = require('../../../services/exercice/data');
+const QuestionData = require('../../../services/question/data');
 
 module.exports = {
   findExamOrReturn: async (req, res, next) => {
@@ -8,6 +10,28 @@ module.exports = {
         return res.status(404).send('Exam Not found');
       }
       res.locals.exam = exam;
+      return next();
+    }
+    return res.status(400).send('Bad Request');
+  },
+  findExerciceOrReturn: async (req, res, next) => {
+    if (req.params.exerciceId) {
+      const exercice = await ExerciceData.getById(req.params.exerciceId);
+      if (!exercice) {
+        return res.status(404).send('Exercice Not found');
+      }
+      res.locals.exercice = exercice;
+      return next();
+    }
+    return res.status(400).send('Bad Request');
+  },
+  findQuestionOrReturn: async (req, res, next) => {
+    if (req.params.questionId) {
+      const question = await QuestionData.getById(req.params.questionId);
+      if (!question) {
+        return res.status(404).send('Question Not found');
+      }
+      res.locals.question = question;
       return next();
     }
     return res.status(400).send('Bad Request');
