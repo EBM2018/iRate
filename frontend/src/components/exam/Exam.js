43dc0dfb98c02cceab3a062a0be42c463a9b3d3e
ValentinChion <valentin.chion@gmail.com>
2019-02-10 10:33:50 +0100

    Create Valentin Chion Branch

---
+++ frontend/src/components/exam/Exam.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import Instructions from "../instructions/Instructions";
+
+export default class Exam extends Component {
+    render() {
+        return (
+            <>
+                <Instructions/>
+            </>
+        );
+    }
+}
---
+++ frontend/src/components/instructions/Instructions.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Instructions extends Component {
+    render() {
+        return (
+            <>
+                <div style={{fontSize: "30px"}}>Indiquez les détails du sujet</div>
+            </>
+        );
+    }
+}
