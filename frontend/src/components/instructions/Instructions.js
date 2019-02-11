4c54689a36ed3169aae5a791b3e9d7e97ecbfbc4
ValentinChion <valentin.chion@gmail.com>
2019-02-11 08:26:49 +0100

    Add handle input func in Instructions Component

--- frontend/src/components/instructions/Instructions.js
+++ frontend/src/components/instructions/Instructions.js
@@ -11,15 +11,23 @@ export default class Instructions extends Component {
         }
     }
 
-
+//trigger the boolean in state with str as input
+//reusable function
     triggerDropdown = (str) => () => {
         this.setState({[str]: !this.state[str]})
+    };
+
+//Put input value in state with name of the input as name of the variable
+//Reusable function
+    handleInput = (e) => {
+        this.setState({[e.target.name]:e.target.value});
     };
 
     render() {
         return (
             <InstructionsDisplayer triggerDropdown={this.triggerDropdown}
                                    dropdownModule={this.state.dropdownModule}
+                                   handleInput={this.handleInput}
                                    dropdownClass={this.state.dropdownClass}/>
         );
     }
