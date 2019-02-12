92e5615291a065cf8d2f8acfb2f7b656db7119af
bdebever <baptiste.debever@gmail.com>
2019-02-12 10:02:26 +0100

    Disable CORS issues

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const serveStatic = require('serve-static');
 const bodyParser = require('body-parser');
@@ -7,9 +8,7 @@ const server = require('http').Server(app);
 
 const config = require('./config');
 
-// setup database connexion
 require('./config/mongoose');
-
 
 app.use(bodyParser.urlencoded({
   extended: false,
@@ -17,14 +16,8 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-
-// Allow Cross-domain origin requests
-// TODO: check safety and make sure it's best way of doing it
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Orsigin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// Enable CORS
+app.use(cors());
 
 app.use('/api', require('./api'));
 
