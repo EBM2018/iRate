de3dd32b06eeace061650ae09cd7db906ecd518d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-12 15:24:33 +0000

    Deploy EBM2018/iRate to github.com/EBM2018/iRate.git:gh-pages

--- api_project.js
+++ api_project.js
@@ -2,14 +2,14 @@ define({
   "title": "iRate API documentation",
   "url": "/api",
   "sampleUrl": "http://localhost:4000/api",
-  "name": "irate",
+  "name": "iRate",
   "version": "1.0.0",
   "description": "An API that handle exams, copies and corrections",
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-02-11T13:06:19.079Z",
+    "time": "2019-02-12T15:23:59.607Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
