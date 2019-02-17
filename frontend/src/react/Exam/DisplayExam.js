d4c4194182858ce3d189f5c831502058d9ee946e
bdebever <baptiste.debever@gmail.com>
2019-02-17 22:21:06 +0100

    First iteration of the displayExam before moving onto its own branch

--- frontend/src/react/Exam/DisplayExam.js
+++ frontend/src/react/Exam/DisplayExam.js
@@ -10,8 +10,10 @@ class DisplayExam extends Component {
     static propTypes = {
         //
     };
+
     componentDidMount() {
-        //this.props.fetchExams();
+        const { id } = this.props.route.match.params;
+        //TODO: get single Exam: this.props.fetchExam();
     }
 
     /*createExam = () => {
@@ -21,6 +23,37 @@ class DisplayExam extends Component {
             'instruction': 'These are some instructions'
         });
     }*/
+
+    render() {
+        const { id } = this.props.route.match.params;
+
+        return(
+            <section id={`exam-${id}`} className="section">
+                <div className="box">
+                    <div className="columns">
+                        <div className="column is-two-thirds">
+                            <span className="title is-5">Module de l'épreuve:</span>
+                        </div>
+                        <div className="column is-one-third has-text-right">
+                            <div>
+                                <div className="dropdown-trigger">
+                                    <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                                        <span>Dropdown button</span>
+                                        <span className="icon is-small"><i className="fas fa-angle-down" aria-hidden="true"/></span>
+                                    </button>
+                                </div>
+                                <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                                    <div className="dropdown-content">
+                                        <div className="dropdown-item">test</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 
 }
 
