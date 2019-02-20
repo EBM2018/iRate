b71056b50039f5a1ddd5d16e28281fa892fc40c3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-20 07:34:14 +0000

    Deploy EBM2018/iRate to github.com/EBM2018/iRate.git:gh-pages

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-02-17T11:27:03.438Z",
+    "time": "2019-02-20T07:33:43.221Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
