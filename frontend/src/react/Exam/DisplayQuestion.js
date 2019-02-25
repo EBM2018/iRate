2222da9612a064c4c986f7fe7c0c0c71af6b6de4
bdebever <baptiste.debever@gmail.com>
2019-02-25 11:52:10 +0100

    Handled the showScale

--- frontend/src/react/Exam/DisplayQuestion.js
+++ frontend/src/react/Exam/DisplayQuestion.js
@@ -1,22 +1,38 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 
-export default class DisplayQuestion extends Component {
+class DisplayQuestion extends Component {
     static propTypes = {
-        question: PropTypes.object.isRequired
+        question: PropTypes.object.isRequired,
+        showScale: PropTypes.bool
+    };
+
+    static defaultProps = {
+        showScale: true
     };
 
     render() {
-        const { question } = this.props;
+        const { question, showScale } = this.props;
         return (
             <div className="level has-background-light py-1 px-1 is-marginless border-bottom">
                 <div className="level-left">
                     {question.title}
                 </div>
-                <div className="level-right">
-                    Echelle: {question.scale}
-                </div>
+                {
+                    showScale &&
+                    (<div className="level-right">
+                        Echelle: {question.scale}
+                    </div>)
+                }
             </div>
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        showScale: state.exams.exams.showScale
+    };
+};
+export default connect(mapStateToProps)(DisplayQuestion);
