2954cedc03836b4f5320d00de09285d4435c557e
ArthurWirbel <wirbel.arthur@gmail.com>
2019-02-27 19:36:09 +0100

    input handleing working

--- frontend/src/react/Exercice/Exercice.js
+++ frontend/src/react/Exercice/Exercice.js
@@ -6,27 +6,8 @@ export default class Exercice extends React.Component {
 
     static propTypes = {
         exercices: PropTypes.array,
-        handleInput: PropTypes.func,
+        handleInputExercice: PropTypes.func,
         id: PropTypes.number,
-    };
-
-  static propTypes = {
-    exercices: PropTypes.array,
-    id: PropTypes.number,
-  };
-
-  state = {
-    question:[
-      {"questionTitle": '', "questionScale": null, "questionContent": ''},
-    ]
-  };
-
-    /**
-     * Put input value in state with name of the input as name of the variable
-     * @param {Object} e
-     */
-    handleInput = (e) => {
-        this.setState({[e.target.name]: e.target.value});
     };
 
     /**
@@ -39,6 +20,31 @@ export default class Exercice extends React.Component {
     };
 
     /**
+     * Put input value in state with name of the input as name of the variable
+     * @param {Object} e
+     */
+    handleInputQuestion = (e) => {
+        let questions = this.state.question;
+        let name = e.target.name;
+        let id = e.target.id;
+        console.log(e.target.id);
+        switch (name) {
+            case 'questionTitle':
+                questions[id].questionTitle = e.target.value;
+                this.setState({question: questions});
+                break;
+            case 'questionScale':
+                questions[id].questionScale = e.target.value;
+                this.setState({question: questions});
+                break;
+            case 'questionContent':
+                questions[id].questionContent = e.target.value;
+                this.setState({question: questions});
+        }
+    };
+
+
+    /**
      * Delete a question related to an exercice.
      *
      * We can't delete the first question.
@@ -47,7 +53,6 @@ export default class Exercice extends React.Component {
      */
     deleteQuestion = (v) => {
         let idQuestion = v.target.value;
-        console.log(idQuestion);
         if (idQuestion === '0') return;
         else {
             const question = [...this.state.question];
@@ -59,8 +64,10 @@ export default class Exercice extends React.Component {
     render() {
         return (
             <div>
-                <ExerciceDisplayer handleInput={this.handleInput}
+                <ExerciceDisplayer handleInputQuestion={this.handleInputQuestion}
+                                   handleInputExercice={this.props.handleInputExercice}
                                    addQuestion={this.addQuestion}
+                                   exercices={this.props.exercices}
                                    deleteQuestion={this.deleteQuestion}
                                    deleteExercice={this.props.deleteExercice}
                                    question={this.state.question}
--- frontend/src/react/Question/Question.js
+++ frontend/src/react/Question/Question.js
@@ -4,27 +4,20 @@ import PropTypes from "prop-types";
 
 export default class Question extends React.Component {
 
-  static propTypes = {
-    exercices: PropTypes.array,
-    deleteQuestion: PropTypes.func,
-    question: PropTypes.array,
-    id: PropTypes.number,
-  };
+    static propTypes = {
+        exercices: PropTypes.array,
+        handleInputQuestion: PropTypes.func,
+        deleteQuestion: PropTypes.func,
+        question: PropTypes.array,
+        id: PropTypes.number,
+    };
 
-  /**
-   * Put input value in state with name of the input as name of the variable
-   * @param {Object} e
-   */
-  handleInput = (e) => {
-    this.setState({[e.target.name]:e.target.value});
-  };
-
-  render() {
-    return (
-      <QuestionDisplayer handleInput={this.handleInput}
-                         deleteQuestion={this.props.deleteQuestion}
-                         question={this.props.question}
-                         id={this.props.id}/>
-    );
-  }
+    render() {
+        return (
+            <QuestionDisplayer handleInput={this.props.handleInputQuestion}
+                               deleteQuestion={this.props.deleteQuestion}
+                               question={this.props.question}
+                               id={this.props.id}/>
+        );
+    }
 }
