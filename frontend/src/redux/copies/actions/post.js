7b185c85d0d979dc9dd8c14be659e62d197a141f
bdebever <baptiste.debever@gmail.com>
2019-03-19 10:31:56 +0100

    Added copy repository and handle copy creation

---
+++ frontend/src/redux/copies/actions/post.js
@@ -0,0 +1,31 @@
+import * as CopiesRepository from '../../../repository/copies';
+
+export const ACTIONS = {
+    SET_POST_COPY_START: 'EXAMS/SET_POST_COPY_START',
+    SET_POST_COPY_SUCCESS: 'EXAMS/SET_POST_COPY_SUCCESS',
+    SET_POST_COPY_FAILURE: 'EXAMS/SET_POST_COPY_FAILURE',
+};
+
+export const SET_POST_COPY_START = () => ({
+    type: ACTIONS.SET_POST_COPY_START,
+});
+
+export const SET_POST_COPY_SUCCESS = copy => ({
+    type: ACTIONS.SET_POST_COPY_SUCCESS,
+    copy,
+});
+
+export const SET_POST_COPY_FAILURE = err => ({
+    type: ACTIONS.SET_POST_COPY_FAILURE,
+    err,
+});
+
+export const postCopy = (copyToAdd) => async dispatch => {
+    dispatch(SET_POST_COPY_START());
+    try {
+        const copy = await CopiesRepository.postCopy(copyToAdd);
+        dispatch(SET_POST_COPY_SUCCESS(copy));
+    } catch (err) {
+        dispatch(SET_POST_COPY_FAILURE(err));
+    }
+};
---
+++ frontend/src/repository/copies.js
@@ -0,0 +1,11 @@
+import {apiRequest} from '../services/api';
+
+export const postCopy = async (copy) => {
+    /*const data = await apiRequest(`/copies`, 'post', {
+        ...copy
+    });*/
+    //return data;
+    return {
+        '_id': 122222222
+    };
+};
