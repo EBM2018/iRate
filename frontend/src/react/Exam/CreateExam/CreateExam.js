034b41a9d31756846bed90bb13ce135a9ca4c793
ValentinChion <valentin.chion@gmail.com>
2019-03-25 09:07:50 +0100

    Add a used component that I deleted just before

---
+++ frontend/src/react/Exam/CreateExam/CreateExam.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import ExerciceList from "./Exercice/ExerciceList/ExerciceList";
+
+export default class CreateExam extends Component {
+    render() {
+        return (
+            <>
+                <div className="tile is-parent">
+                    <div className="tile is-child">
+                        <ExerciceList id={this.props.route.match.params.examId}/>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
