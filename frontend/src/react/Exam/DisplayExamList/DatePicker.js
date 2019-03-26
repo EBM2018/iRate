af25f9c1213b2551e9c830bf931cd884baec9052
ValentinChion <valentin.chion@gmail.com>
2019-03-26 09:36:49 +0100

    Add DatePicker options to ExamList

---
+++ frontend/src/react/Exam/DisplayExamList/DatePicker.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import DayPicker from 'react-day-picker';
+
+export default class DatePicker extends Component {
+  render() {
+    const {
+      simplified,
+      handleSimplified,
+      handleDayChange,
+      day
+    } = this.props;
+
+    return (
+      <div className="is-fixed-bottom-right">
+        {simplified ?
+          <button className="button is-medium is-rounded is-info" onClick={handleSimplified}>
+            Filtrer par date
+          </button>
+          :
+          <aside className=" box">
+            <p className="subtitle">Filtrer sur le calendrier</p>
+            <DayPicker onDayClick={handleDayChange} selectedDays={day ? day : undefined}/>
+          </aside>}
+      </div>
+    );
+  }
+}
--- frontend/src/react/Exam/DisplayExamList/ExamList.js
+++ frontend/src/react/Exam/DisplayExamList/ExamList.js
@@ -5,61 +5,96 @@ import ExamListDisplayer from './ExamListDisplayer';
 import {getExams} from '../../../redux/exams/actions/get';
 import FinaliseExamDisplayer from './FinaliseExam/FinaliseExamDisplayer';
 import {patchExam} from '../../../redux/exams/actions/patch';
-import {sortExamsBySessionDate} from "../../../helpers/exam";
+import {filterNotPassedExams, filterPassedExams, sortExamsBySessionDate} from '../../../helpers/exam';
+import DatePicker from './DatePicker';
 
 class ExamList extends Component {
-    state = {
-        shouldFinaliseRender: false,
-        didChecked: false,
-        examId: '',
-    };
+  state = {
+    shouldFinaliseRender: false,
+    didChecked: false,
+    examId: '',
+    simplified: true,
+    day: '',
+  };
 
-    async componentDidMount() {
-        await this.props.fetchExamsWithScale();
-        if(this.props.exams) {
-            const exams = sortExamsBySessionDate(this.props.exams);
-            this.setState({exams});
+  async componentDidMount() {
+    await this.props.fetchExamsWithScale();
+    if(this.props.exams) {
+      const exams = await sortExamsBySessionDate(this.props.exams);
+      const passedExams = await sortExamsBySessionDate(filterPassedExams(this.props.exams));
+      const notPassedExams = await sortExamsBySessionDate(filterNotPassedExams(this.props.exams));
+      const finalisedExams = await notPassedExams.filter(exam => {
+        return exam.isFinalised
+      });
+      const createdExams = await sortExamsBySessionDate(exams.filter(exam => {
+        return !exam.isFinalised
+      }));
+      this.setState({passedExams,finalisedExams,createdExams});
+    }
+  }
+
+  filterByCalendarDate() {
+    const { day } = this.state;
+    if (day) {
+
+    }
+  }
+
+  toggleFinalise = (id) => () => {
+    this.setState({shouldFinaliseRender: !this.state.shouldFinaliseRender, examId: id});
+  };
+
+  toggleCheckScale = () => {
+    this.setState({didChecked: !this.state.didChecked});
+  };
+
+  finaliseExam = (id) => async () => {
+    const exam = await this.props.exams.filter((exam) => {return exam._id === id})[0];
+    exam.isFinalised = true;
+    exam.showScale = this.state.didChecked;
+    await this.props.fetchExamPatcher(exam);
+    await this.toggleFinalise('')();
+  };
+
+  handleSimplified = () => {
+    let {simplified} = this.state;
+    simplified = !simplified;
+    this.setState({simplified})
+  };
+
+  handleDayChange = (day) => {
+    this.setState({day});
+  };
+
+  render() {
+    return (
+      <>
+        {
+          this.props.loading &&
+          `loading`
         }
-    }
-
-    toggleFinalise = (id) => () => {
-        this.setState({shouldFinaliseRender: !this.state.shouldFinaliseRender, examId: id});
-    };
-
-    toggleCheckScale = () => {
-        this.setState({didChecked: !this.state.didChecked});
-    };
-
-    finaliseExam = (id) => async () => {
-        const exam = await this.props.exams.filter((exam) => {return exam._id === id})[0];
-        exam.isFinalised = true;
-        exam.showScale = this.state.didChecked;
-        await this.props.fetchExamPatcher(exam);
-        await this.toggleFinalise('')();
-    };
-
-    render() {
-        return (
-            <>
-                {
-                    this.props.loading &&
-                    `loading`
-                }
-                {this.state.exams ? <ExamListDisplayer exams={this.state.exams} toggleFinalise={this.toggleFinalise}/> : 'waiting'}
-                {this.state.shouldFinaliseRender ? <FinaliseExamDisplayer toggleFinalise={this.toggleFinalise}
-                                                                          id={this.state.examId}
-                                                                          toggleScaleCheck={this.toggleCheckScale}
-                                                                          didChecked={this.state.didChecked}
-                                                                          finaliseExam={this.finaliseExam}/> : null}
-            </>
-        );
-    }
+        {this.state.createdExams ? <ExamListDisplayer passedExams={this.state.passedExams}
+                                                      finalisedExams={this.state.finalisedExams}
+                                                      createdExams={this.state.createdExams}
+                                                      toggleFinalise={this.toggleFinalise}/> : 'waiting'}
+        <DatePicker simplified={this.state.simplified}
+                    handleSimplified={this.handleSimplified}
+                    handleDayChange={this.handleDayChange}
+                    day={this.state.day}/>
+        {this.state.shouldFinaliseRender ? <FinaliseExamDisplayer toggleFinalise={this.toggleFinalise}
+                                                                  id={this.state.examId}
+                                                                  toggleScaleCheck={this.toggleCheckScale}
+                                                                  didChecked={this.state.didChecked}
+                                                                  finaliseExam={this.finaliseExam}/> : null}
+      </>
+    );
+  }
 }
 
 export default connect(state => ({
-    exams: state.exams.exams,
-    loading: state.exams.loading,
+  exams: state.exams.exams,
+  loading: state.exams.loading,
 }), dispatch => ({
-    fetchExamsWithScale: () => dispatch(getExams({}, true)),
-    fetchExamPatcher: (exam) => dispatch(patchExam(exam))
+  fetchExamsWithScale: () => dispatch(getExams({}, true)),
+  fetchExamPatcher: (exam) => dispatch(patchExam(exam))
 }))(ExamList);
