56e79b755835846d6686839b829f0b0a0edbeb10
ValentinChion <valentin.chion@gmail.com>
2019-03-30 19:05:41 +0100

    Disable navbar for homepage style

--- frontend/src/react/Layout/LayoutApp.js
+++ frontend/src/react/Layout/LayoutApp.js
@@ -1,32 +1,40 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-import "./LayoutApp.css";
-import Navbar from "./Navbar";
+import './LayoutApp.css';
+import Navbar from './Navbar';
 
 export default class LayoutApp extends Component {
   static propTypes = {
     component: PropTypes.func.isRequired,
     title: PropTypes.string.isRequired,
-    route: PropTypes.object
+    route: PropTypes.object,
   };
 
   render() {
     const Component = this.props.component;
-    const { title, route } = this.props;
-
+    const {title, route} = this.props;
+    const {path} = this.props.route.match;
     return (
-      <div>
-        {/** This navbar can be extended and use as a menu, we can also add sidebar */}
-        <Navbar title={title} />
-        <main>
-          <div className="container py-1">
-            <Component route={route} />
+      <>
+        {path !== '/' ?
+          <div>
+            {/** This navbar can be extended and use as a menu, we can also add sidebar */}
+            <Navbar title={title}/>
+              <main>
+                <div className="container py-1">
+                  <Component route={route}/>
+                  {this.props.loading && `loading`}
+                </div>
+              </main>
+          </div> :
+          <>
+            <Component route={route}/>
             {this.props.loading && `loading`}
-          </div>
-        </main>
+          </>
+        }
         {/** We can add footer here */}
-      </div>
+      </>
     );
   }
 }
